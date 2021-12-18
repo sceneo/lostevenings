f0fe8ea67f08f88721016f726c7ec503b737f4a2
Tobias Kunz <Tobias.Kunz@tngtech.com>
2021-12-18 21:22:12 +0100

    first poc

---
+++ src/Constants.ts
@@ -0,0 +1,2 @@
+
+export const LOST_EVENINGS_START: Date = new Date ( 2022, 8, 15, 20, 0, 0, 0 );
---
+++ src/calculator/TimeSpan.ts
@@ -0,0 +1,11 @@
+
+export interface TimeSpan {
+    days: number;
+    hours: number;
+    minutes: number;
+    seconds: number;
+    totalSeconds: number;
+    totalMinutes: number;
+    totalHours: number;
+    totalDays: number;
+}
